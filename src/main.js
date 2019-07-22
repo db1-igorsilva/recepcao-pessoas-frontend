9169f59251ee7446f3fc9760dea7ce0c5fadec7e
db1-igorsilva <igor.silva@db1.com.br>
2019-07-22 12:07:56 -0300

    import { routes } from './routes';

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import 'vuetify/dist/vuetify.min.css'
 import Vuetify from 'vuetify';
 import VueRouter from 'vue-router';
+import { routes } from './routes';
 
 Vue.use(Vuetify);
 Vue.use(VueRouter);
