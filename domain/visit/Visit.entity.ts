b6b605d3c7b60b990e5c9fd6af22950af143393c
db1-igorsilva <igor.silva@db1.com.br>
2019-07-22 13:32:22 -0300

    Visit.entity.ts constructor

--- domain/visit/Visit.entity.ts
+++ domain/visit/Visit.entity.ts
@@ -1,3 +1,14 @@
 export default class Visit {
-    
+    id;
+    date;
+    start_time;
+    end_time;
+    guest;
+    constructor(id, date, start_time, end_time, guest) {
+        this.id = id;
+        this.date = date;
+        this.start_time = start_time;
+        this.end_time = end_time;
+        this.guest = guest;
+    }
 }
