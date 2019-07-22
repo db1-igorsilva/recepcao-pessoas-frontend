ec64db11f82a1d0e5fbeeb1a63444ccb7e6e88cf
db1-igorsilva <igor.silva@db1.com.br>
2019-07-22 12:14:24 -0300

    Default route added

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Cadastro from '../views/cadastro/Cadastro.view.vue';
 import Consulta from '../views/consulta/Consulta.view.vue';
 export const routes = [
-    { path: '', component: Consulta },
-    { path: '/cadastro', component: Cadastro }
+    { path: '/consulta', component: Consulta },
+    { path: '/cadastro', component: Cadastro },
+    { path: '*', component: Consulta }
 ];
