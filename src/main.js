6225f435abf82c2cfe00cd86f204f857ba615864
db1-igorsilva <igor.silva@db1.com.br>
2019-07-22 12:03:33 -0300

    VueRouter imported from 'vue-router'

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify';
 import App from './App.vue'
 import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify';
+import VueRouter from 'vue-router';
 
 Vue.use(Vuetify);
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
