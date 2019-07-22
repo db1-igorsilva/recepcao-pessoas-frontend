c0cd89ebc319e1a2b6dda0236e6531ea77ec1e5b
db1-igorsilva <igor.silva@db1.com.br>
2019-07-22 12:06:37 -0300

    route to Consulta added

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,5 @@
 import Cadastro from '../views/cadastro/Cadastro.view.vue';
 import Consulta from '../views/consulta/Consulta.view.vue';
-export const routes = [];
+export const routes = [
+    { path: '', component: Consulta }
+];
