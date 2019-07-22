7042c66d3dff78aab77f185b93d6d25ef9ea1f50
db1-igorsilva <igor.silva@db1.com.br>
2019-07-22 10:42:06 -0300

    v-toolbar added

---
+++ shared/menu/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-toolbar>
+        <v-toolbar-side-icon> </v-toolbar-side-icon>
+        <v-toolbar-title> DB1 Global Software <v-toolbar-title>
+            <v-spacer/>
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>
+                <router-link> Consulta </router-link>
+            </v-btn>
+            <v-btn flat>
+                <router-link> Cadastro </router-link>
+            </v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="sass">
+</style>
