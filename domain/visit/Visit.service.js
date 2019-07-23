4d2bf7ce3e73ebc928a046da4439a217ea46a06b
db1-igorsilva <igor.silva@db1.com.br>
2019-07-23 15:30:01 -0300

    Visit.service.js delete method changed

--- domain/visit/Visit.service.js
+++ domain/visit/Visit.service.js
@@ -3,13 +3,7 @@ import { http } from '../../src/main.js';
 export default {
     
     save:(visit) => {
-        if (visit._id) {
-            return this._resource
-                .upadte({ id: visit._id }, visit);
-        } else {
-            return this._resource
-                .save(visit);
-        }
+        return http.post('post', visit);
     },
     getAll:() => {
         return http.get('getAll');
@@ -19,12 +13,7 @@ export default {
             .get({ id })
             .then(getResponse => getResponse.json());
     },
-    delete:(id) => {
-        return this._resource
-            .delete({ id })
-            .then(null, error => {
-                console.log(error);
-                throw new Error('Visit can\'t be removed.');
-            });
+    delete:(visit) => {
+        return http.delete('', { data: visit.id })
     }
 }
