4466e0314c9561f65334b0ad0c73989c2447ec26
db1-igorsilva <igor.silva@db1.com.br>
2019-07-23 11:34:51 -0300

    VeeValidate import and use on main.js

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import 'vuetify/dist/vuetify.min.css'
 import Vuetify from 'vuetify';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
+import VeeValidate from 'vee-validate';
 import { routes } from './routes';
 
 Vue.use(Vuetify);
 Vue.use(VueRouter);
 Vue.use(VueResource);
+Vue.use(VeeValidate);
 Vue.http.options.root = 'http://localhost:3000';
 
 const router = new VueRouter( {
