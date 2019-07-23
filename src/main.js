79b4ef6579cab96bcb0764c25cfa752104e96f59
db1-igorsilva <igor.silva@db1.com.br>
2019-07-23 11:33:59 -0300

    VueResoruce import and use fixed (from VueResoruce to VueResource)

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import 'vuetify/dist/vuetify.min.css'
 import Vuetify from 'vuetify';
 import VueRouter from 'vue-router';
-import VueResoruce from 'vue-resource';
+import VueResource from 'vue-resource';
 import { routes } from './routes';
 
 Vue.use(Vuetify);
 Vue.use(VueRouter);
-Vue.use(VueResoruce);
+Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
 
 const router = new VueRouter( {
