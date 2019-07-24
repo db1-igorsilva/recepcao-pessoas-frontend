cb7e674552b5bb62c5056d90b8539c80592660cf
db1-igorsilva <igor.silva@db1.com.br>
2019-07-24 10:01:44 -0300

    Update method merged to save method

--- domain/visit/Visit.service.js
+++ domain/visit/Visit.service.js
@@ -3,13 +3,13 @@ import { http } from '../../src/main.js';
 export default {
     
     save:(visit) => {
-        return http.post('post', visit);
+        if(visit.id) {
+            return http.post('post', visit);
+        }
+        return http.put(visit.id, visit);
     },
     getAll:() => {
         return http.get('getAll');
-    },
-    update:(visit) => {
-        return http.put(visit.id, visit);
     },
     delete:(visit) => {
         return http.delete(visit.id);
