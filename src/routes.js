ac0c4331a30d8187c72fab9cfdb94d7b00e73a1d
db1-igorsilva <igor.silva@db1.com.br>
2019-07-24 09:47:26 -0300

    /cadastro/:id splitted from /cadastro

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Cadastro from '../views/cadastro/Cadastro.view.vue';
 import Consulta from '../views/consulta/Consulta.view.vue';
 export const routes = [
     { path: '/consulta', component: Consulta, name: 'consulta' },
-    { path: '/cadastro/:id', component: Cadastro, name: 'cadastro' },
+    { path: '/cadastro', component: Cadastro, name: 'cadastro' },
+    { path: '/cadastro/:id', component: Cadastro, name: 'altera' },
     { path: '*', component: Consulta, name: 'consulta' }
 ];
