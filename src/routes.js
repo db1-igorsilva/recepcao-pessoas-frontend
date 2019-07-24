65ebc63c9dcea6b54f65335acc9a270bbd25e5a1
db1-igorsilva <igor.silva@db1.com.br>
2019-07-24 09:41:07 -0300

    Cadastro path changed from '/cadastro' to '/cadastro/:id'

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import Cadastro from '../views/cadastro/Cadastro.view.vue';
 import Consulta from '../views/consulta/Consulta.view.vue';
 export const routes = [
     { path: '/consulta', component: Consulta, name: 'consulta' },
-    { path: '/cadastro', component: Cadastro, name: 'cadastro' },
+    { path: '/cadastro/:id', component: Cadastro, name: 'cadastro' },
     { path: '*', component: Consulta, name: 'consulta' }
 ];
