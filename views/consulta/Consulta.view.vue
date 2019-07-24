92c9e0213c1ee558a88913ac0b3978d021210f10
db1-igorsilva <igor.silva@db1.com.br>
2019-07-24 12:37:26 -0300

    All filters working

--- views/consulta/Consulta.view.vue
+++ views/consulta/Consulta.view.vue
@@ -4,6 +4,8 @@
             <v-flex class="not_full_width" @input="typed_filter = $event.target.value">
                 <v-text-field label="Nome do Visitante"/>
             </v-flex>
+            <input type="date" id="compareStartDate" name="compareStartDate" @input="compareStartDate = $event.target.value"/>
+            <input type="date" id="compareEndDate" name="compareEndDate" @input="compareEndDate = $event.target.value"/>
             <v-data-table :headers="headers" :items="filteredVisits" class="not_full_width">
                 <template v-slot:items="filteredVisits">
                     <td class="text-xs-right"> {{ filteredVisits.item.id }} </td>
@@ -62,14 +64,23 @@ export default {
             }
         ],
         visits: [],
-        typed_filter: ''
+        typed_filter: '',
+        compareStartDate: '',
+        compareEndDate: ''
       }
     },
     computed: {
         filteredVisits() {
+            if (this.compareStartDate && this.compareEndDate && this.typed_filter) {
+                let wordFilter = new RegExp(this.typed_filter.trim(), 'i');
+                return this.visits.filter(visit => visit.date >= this.compareStartDate && visit.date <= this.compareEndDate).filter(visit => wordFilter.test(visit.guest));
+            }
+            if (this.compareStartDate && this.compareEndDate) {
+                return this.visits.filter(visit => visit.date >= this.compareStartDate && visit.date <= this.compareEndDate);
+            }
             if (this.typed_filter) {
-                let filtered = new RegExp(this.typed_filter.trim(), 'i')
-                return this.visits.filter(visit => filtered.test(visit.guest));
+                let wordFilter = new RegExp(this.typed_filter.trim(), 'i')
+                return this.visits.filter(visit => wordFilter.test(visit.guest));
             }
             return this.visits;
         }
