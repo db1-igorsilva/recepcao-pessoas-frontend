80f018037f706c629ed824dedf5450d42ea3c4e9
db1-igorsilva <igor.silva@db1.com.br>
2019-07-25 12:21:44 -0300

    Menu's design changed

--- shared/menu/Menu.vue
+++ shared/menu/Menu.vue
@@ -1,14 +1,14 @@
 <template>
     <v-toolbar color="#8F1C2C" class="menu__header">
         <v-toolbar-side-icon> <v-img src="https://db1global.sharepoint.com/sites/intranet/Central%20de%20documentos%20PDF/Imagens%20Oficiais/ORG_LOGO_DB1_BRANCO.png"/> </v-toolbar-side-icon>
-        <v-toolbar-title> DB1 Global Software </v-toolbar-title>
-            <v-spacer/>
+        <v-toolbar-title class="title"> DB1 Global Software </v-toolbar-title>
+        <v-spacer/>
         <v-toolbar-items>
-            <v-btn flat>
-                <router-link to="/consulta"> Consulta </router-link>
+            <v-btn flat class="menu__header">
+                <router-link to="/consulta" class="menu__header_button"> Consulta </router-link>
             </v-btn>
-            <v-btn flat>
-                <router-link to="/cadastro"> Cadastro </router-link>
+            <v-btn flat class="menu__header">
+                <router-link to="/cadastro" class="menu__header_button"> Cadastro </router-link>
             </v-btn>
         </v-toolbar-items>
     </v-toolbar>
@@ -23,4 +23,15 @@ export default {
 <style lang="sass">
 .menu__header
     height: 100px
+    &_db1infos
+        width: 75%
+    &_title
+        display: flex
+        flex-direction: row
+    &_button
+        display: flex
+        flex-direction: row
+        font-size: 1.5em
+        text-decoration: none
+        color: #000
 </style>
