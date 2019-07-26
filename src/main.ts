726b569aeb6a1cd93a0a50b9559f1a9cad57f70f
db1-igorsilva <igor.silva@db1.com.br>
2019-07-26 15:49:53 -0300

    Fast commit

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import 'vuetify/dist/vuetify.min.css'
-import Vuetify from 'vuetify';
-import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
-import VeeValidate from 'vee-validate';
-import { routes } from './routes';
+import Vue from 'vue';
+import './plugins/vuetify'
+import App from './App.vue';
+import router from './router';
 import axios from 'axios';
-
-Vue.use(Vuetify);
-Vue.use(VueRouter);
-Vue.use(VueResource);
-Vue.use(VeeValidate);
-Vue.http.options.root = 'http://localhost:3000';
-
-const router = new VueRouter( {
-  routes,
-  mode: 'history'
-} );
 
 export const http = axios.create({
   baseURL: 'http://localhost:3000/'
 });
 
 new Vue({
-  el: '#app',
   router,
   render: h => h(App)
-})
+}).$mount('#app');
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,17 @@
-import NewVisit from '../views/new-visit/NewVisit.view.vue';
-import Visits from '../views/visits/Visits.view.vue';
-export const routes = [
-    { path: '/', component: Visits, name: 'visits' },
-    { path: '/new_visit', component: NewVisit, name: 'new_visit' },
-    { path: '/new_visit/:id', component: NewVisit, name: 'update' },
-    { path: '*', component: Visits, name: 'visits' }
-];
+import Vue from 'vue';
+import Router from "vue-router";
+import NewVisit from '@/views/new-visit/NewVisit.view.vue';
+import Visits from '@/views/visits/Visits.view.vue';
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes : [
+        { path: '/', component: Visits, name: 'visits' },
+        { path: '/new_visit', component: NewVisit, name: 'new_visit' },
+        { path: '/new_visit/:id', component: NewVisit, name: 'update' },
+        { path: '*', redirect: '/' },
+    ]
+});
