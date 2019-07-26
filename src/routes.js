047178cd1df9cc2391c20662d20c4ed0fbdd4a2b
db1-igorsilva <igor.silva@db1.com.br>
2019-07-26 11:42:20 -0300

    Routes paths and names translated to english

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import Cadastro from '../views/cadastro/Cadastro.view.vue';
 import Consulta from '../views/consulta/Consulta.view.vue';
 export const routes = [
-    { path: '/', component: Consulta, name: 'consulta' },
-    { path: '/cadastro', component: Cadastro, name: 'cadastro' },
-    { path: '/cadastro/:id', component: Cadastro, name: 'altera' },
-    { path: '*', component: Consulta, name: 'consulta' }
+    { path: '/', component: Consulta, name: 'visits' },
+    { path: '/new_visit', component: Cadastro, name: 'new_visit' },
+    { path: '/new_visit/:id', component: Cadastro, name: 'update' },
+    { path: '*', component: Consulta, name: 'visits' }
 ];
