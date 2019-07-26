19a4be71bbac1f8971b9946a3f0ceac570cefcd7
db1-igorsilva <igor.silva@db1.com.br>
2019-07-26 11:44:12 -0300

    Routes imports and components translated to english

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-import Cadastro from '../views/cadastro/Cadastro.view.vue';
-import Consulta from '../views/consulta/Consulta.view.vue';
+import NewVisit from '../views/cadastro/Cadastro.view.vue';
+import Visits from '../views/consulta/Consulta.view.vue';
 export const routes = [
-    { path: '/', component: Consulta, name: 'visits' },
-    { path: '/new_visit', component: Cadastro, name: 'new_visit' },
-    { path: '/new_visit/:id', component: Cadastro, name: 'update' },
-    { path: '*', component: Consulta, name: 'visits' }
+    { path: '/', component: Visits, name: 'visits' },
+    { path: '/new_visit', component: NewVisit, name: 'new_visit' },
+    { path: '/new_visit/:id', component: NewVisit, name: 'update' },
+    { path: '*', component: Visits, name: 'visits' }
 ];
